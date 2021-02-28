f94993de2e71a856ff5aea1599dd3702d4d330eb
Nicholas Matthew Dharmadi <sc19nmd@leeds.ac.uk>
2021-02-28 13:47:46 +0000

    added templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Power-Rangers!"
+    user = {'name': 'Power-Rangers'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
